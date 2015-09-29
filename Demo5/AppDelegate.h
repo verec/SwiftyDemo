a58b53d3d8015b5f60a8a221f5ee3f7e9e56c497
Jean-Francois Brouillet <verec@mac.com>
2015-09-29 22:16:30 +0100

    Initial Commit

---
+++ Demo5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo5
+//
+//  Created by verec on 29/09/2015.
+//  Copyright © 2015 Cantabilabs Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
